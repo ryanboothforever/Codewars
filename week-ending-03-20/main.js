39dd9c8853b6f7c0177b770343daa1e41855e191
Ryan Booth <ryanboothforever@gmail.com>
2021-03-16 17:14:53 -0500

    More Wars

--- week-ending-03-20/main.js
+++ week-ending-03-20/main.js
@@ -2,3 +2,21 @@
 function makeUppercase(str){
     return str.toUpperCase();
 }
+
+//8 Kyu: Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!). (The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
+
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  }
+  if (array.length <= 2) {
+    return 0;
+  } else {
+    array = array.sort((a, b) => a - b);
+    array = array.slice(1, -1);
+    let theSum = array.reduce((acc, res) => {
+      return acc + res;
+    });
+    return theSum;
+  }
+}
