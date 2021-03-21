a6b9580a20ad1edcc191c67c65e9875350eb4615
Ryan Booth <ryanboothforever@gmail.com>
2021-03-21 08:22:03 -0500

    Code wars conditional

--- week-ending-06-26/main.js
+++ week-ending-06-26/main.js
@@ -76,3 +76,13 @@ function addFive(num) {
   var total = num + 5;
   return total;
 }
+
+/* 8 Kyu: Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0. */
+
+function combat(health, damage) {
+  let newHealth = health - damage;
+  if (newHealth < 0) {
+    return 0;
+  }
+  return newHealth;
+}
