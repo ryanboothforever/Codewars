9674f4f06f65934866bbdf6bbddc4e9444c71386
Ryan Booth <ryanboothforever@gmail.com>
2021-03-27 09:21:22 -0500

    New wars

--- week-ending-06-12/main.js
+++ week-ending-06-12/main.js
@@ -90,3 +90,9 @@ function between(a, b) {
   }
   return list;
 }
+
+/* 8 Kyu: This function should test if the factor is a factor of base. Return true if it is a factor or false if it is not.*/
+
+function checkForFactor(base, factor) {
+  return base % factor === 0 ? true : false;
+}
