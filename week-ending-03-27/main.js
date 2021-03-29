b2732b87f52c0f5133ce64b7b7d41c1d8499c096
Ryan Booth <ryanboothforever@gmail.com>
2021-03-29 07:00:31 -0500

    New week of wars

--- week-ending-03-27/main.js
+++ week-ending-03-27/main.js
@@ -8,4 +8,16 @@ function countBy(x, n) {
   return z;
 }
 
-console.log(countBy(2,5));
+/* 8 Kyu:  There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!*/
+
+function betterThanAverage(classPoints, yourPoints) {
+  let avg = classPoints.reduce((acc, rec)=> {
+    return acc+rec;
+  }, 0)
+  return avg/classPoints.length > yourPoints ? false : true;
+}
