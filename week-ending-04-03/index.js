4990ad0c6225dff1fa442142b66b533f7de7a0e3
Ryan Booth <ryanboothforever@gmail.com>
2021-04-01 12:25:45 -0500

    Even MORE wars

--- week-ending-04-03/index.js
+++ week-ending-04-03/index.js
@@ -20,3 +20,11 @@ class Kata {
     return length * width * height;
   }
 }
+
+// 8 Kyu: Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+function smash (words) {
+    return words.join(" ").trim()
+};
+
+console.log(smash([" hi", "there"]));
