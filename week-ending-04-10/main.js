04927fa68111af474cef3f4a6fcc32e7be645840
Ryan Booth <ryanboothforever@gmail.com>
2021-04-06 07:24:46 -0500

    One more for today

--- week-ending-04-10/main.js
+++ week-ending-04-10/main.js
@@ -49,3 +49,14 @@ function functionisDivideBy(num, a, b){
     return num%a === 0  && num%b === 0 ? true : false
 }
 
+// 7 Kyu: Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/ig, "");
+}
+
+/* 8 Kyu: You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around! Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail). Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics */
+
+function fixTheMeerkat(arr) {
+  return arr.reverse()
+}
