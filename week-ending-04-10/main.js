b090f3b61434b607b5e3a9a139340abd3a45ad59
Ryan Booth <ryanboothforever@gmail.com>
2021-04-10 09:04:40 -0500

    Last wars of the week 04-10

--- week-ending-04-10/main.js
+++ week-ending-04-10/main.js
@@ -87,3 +87,12 @@ function points(games) {
   return x
 }
 
+// 8 Kyu: Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+function doubleChar(str) {
+  let newStr = ""
+  for (let i = 0; i < str.length; i++) {
+      newStr += str[i] + str[i];
+  }
+  return newStr
+}
