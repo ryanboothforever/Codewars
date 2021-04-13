f5f879b6740bb1724842237cc6e5b5361286b4c5
Ryan Booth <ryanboothforever@gmail.com>
2021-04-13 06:17:15 -0500

    Wars added for today

--- week-ending-04-17/main.js
+++ week-ending-04-17/main.js
@@ -12,4 +12,12 @@ const square = (num) => {
   return num ** 2;
 };
 
+// 8 Kyu: You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
 
+function index(arr, n){
+  if(n < arr.length){
+    return arr[n]**n;
+  } else {
+    return -1
+  }
+}
---
+++ week-ending-04-17/timed-thing.js
@@ -0,0 +1,98 @@
+/*Checking for Compromised Accounts
+Overview
+We want to inform our users if their account has been involved in a security breach on another site since their last login. Since people sometimes reuse passwords across multiple sites, we'll encourage them to update their information in case their password is no longer secure.
+
+Tasks
+You'll see the current login logic in src/login.js with some hardcoded values for testing purposes. Your goal is to get this code hitting an external breach data API called Hack Check.
+
+Please follow the steps below in order. This should help you to make efficient use of the time allotted.
+
+Look for recent breaches that involve passwords. Limit the breaches in sampleResponse to breaches where:
+
+IsSensitive is false
+DataClasses array contains the value 'Passwords'
+AddedDate is after the user's lastLogin
+Should breaches be present upon successful user login, return the following as formatted:
+
+ {
+   success: true,
+   meta: {
+     suggestPasswordChange: true,
+     breachedAccounts: [{
+       name: <Name>,
+       domain: <Domain>,
+       breachDate: <BreachDate>,
+       addedDate: <AddedDate>
+     }]
+   }
+ }
+After this is working with the hardcoded test values, move on to incorporate real breach data using the Hack Check breachedaccount API.
+
+Test users
+Before hitting the API, the app is hardcoded to return the same 3 breaches for either user.
+
+User	Email	Password	API response
+Safe User	safe@example.com	pw	no breaches found (HTTP 404)
+Unsafe User	test@example.com	pw	some breaches found (HTTP 200)
+Other notes
+No UI changes needed: You should not modify any user interface code.
+
+Error handling: Only light API error handling is expected. Note that the API will respond with HTTP 404 if the user has no known breaches.
+
+Testing: Open the Web Preview. There will be a login form and a box below it that shows the result of the last login attempt. We will not evaluate changes to the automated tests.
+
+If you see Web Preview Qualified 404 errors, please disable any content blockers you may have running and try reloading the page.
+If you have API issues, double-check your request URL and API endpoint.
+Javascript Console: To see logging output for the Web Preview, click on the web preview console button:
+image.png
+
+To use a debugger or see detailed console logs, open your browser's Javascript console / devtools.
+
+Time guidance
+Set a timer for 40 minutes; however the scenario should take 25-35 minutes to complete. If you exceed 40 minutes:
+
+STOP
+In "Your Notes" (in Qualified) note any tradeoffs or things you would do differently given more time.
+If this is not the last scenario, go on to the next scenario. It is independent from any solution you got on this scenario.
+You can return to any incomplete scenarios if you have time left over at the end.*/
+
+import {
+  users as sampleUsers,
+  breaches as sampleBreaches,
+  response as sampleResponse,
+} from "./sample";
+
+function authenticate(email, password) {
+  const account = sampleUsers.find((a) => a.email === email);
+  if (account && account.password === password) {
+    return account;
+  } else {
+    return null;
+  }
+}
+
+async function login(email, password) {
+  const account = authenticate(email, password);
+  if (account) {
+    // A new breach was detected!
+    if (sampleResponse.length > 0) {
+      return {
+        success: true,
+        meta: {
+          suggestPasswordChange: true,
+          // hardcoded for now...
+          breachedAccounts: sampleBreaches,
+        },
+      };
+    } else {
+      return { success: true };
+    }
+  } else {
+    return {
+      success: false,
+      message: "The username or password you entered is invalid.",
+    };
+  }
+}
+
+export default login;
