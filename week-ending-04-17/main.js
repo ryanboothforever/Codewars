894a69e00c62fff3807db192a2697cd1e745ec19
Ryan Booth <ryanboothforever@gmail.com>
2021-04-14 10:22:55 -0500

    Added preFizz codewar

--- week-ending-04-17/main.js
+++ week-ending-04-17/main.js
@@ -168,3 +168,19 @@ function dutyFree(normPrice, discount, hol) {
   }
   return Math.floor(hol/priceForBottles)
 }
+
+/* 8 Kyu: This is the first step to understanding FizzBuzz. Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value. Your expected output is an array of positive integers from 1 to n (inclusive). Your job is to write an algorithm that gets you from the input to the output.*/
+
+function preFizz(n) {
+  arr = []
+  if(n.length === 0){
+    return []
+  } else {
+  for(let i = 1; i<=n; i++){
+    arr.push(i)
+  }
+}
+return arr
+}
+
+console.log(preFizz("woof"));
