3b210f696086f9d33a8b29aa40676278bc5c0c05
Ryan Booth <ryanboothforever@gmail.com>
2021-04-15 08:40:34 -0500

    Thursday of this week code wars

--- week-ending-04-17/main.js
+++ week-ending-04-17/main.js
@@ -21,3 +21,51 @@ function index(arr, n){
     return -1
   }
 }
+
+// 8 Kyu: Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+function removeEveryOther(arr){
+  let newArr = arr.filter((item, i) => {
+    if (item[i]===0){
+      return item
+    }
+  else if(i%2===0){
+     return item
+   }
+  })
+  return newArr
+}
+
+// 8 Kyu: Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".  The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct. Upper or lower case letter does not matter -- "eNglisH" is also correct.  Return value as boolean values, true for the string to contains "English", false for it does not.
+
+function spEng(sentence) {
+  let english = "english"
+  sentence = sentence.toLowerCase()
+  return sentence.includes(english) ? true : false;
+}
+
+//Write a function to get the first elements of asequence. Passing a parameter n (default=1) will return the first n elements of the sequence. If n == 0 return an empty sequence []
+function first(arr, n=1) {
+  let newArr = [];
+  if(arr.length < n){
+    n = arr.length
+  }
+  for (let i = 0; i < n; i++) {
+     newArr.push(arr[i]);
+  }
+  if (n === 0){
+    return [];
+  } else {
+    return newArr;
+  }
+}
+
+// Much better solution
+const first1 = (arr, n = 1) => arr.slice(0, n);
+
+// 8 Kyu: Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him. Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :) If Alex gets 10 or more hoops, return the string "Great, now move on to tricks". If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+function hoopCount(n) {
+  return n < 10
+    ? "Keep at it until you get it"
+    : "Great, now move on to tricks";
+}
