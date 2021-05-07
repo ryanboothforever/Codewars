2cd30666829e94fc773afb6173069cc64d77c829
Ryan Booth <ryanboothforever@gmail.com>
2021-05-07 14:03:35 -0500

    Last wars of the week 05-08

--- week-ending-05-08/main.js
+++ week-ending-05-08/main.js
@@ -12,4 +12,44 @@ function well(x) {
     ? "Fail!"
     : "Publish!";
 }
-console.log(well(["bad", "bad", "good", "good", "good"]));
+
+/* 8 Kyu: Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1 */
+
+function correct(string) {
+  return string.replace(/0/gi, "O").replace(/1/gi, "I").replace(/5/gi, "S");
+}
+
+/* 8 Kyu: This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?*/
+//removed "=" so it wouldn't compare
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
+/* 8 Kyu: Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+Example: (Input1, Input2 -->Output) */
+
+function sumStr(a, b) {
+  let total = Number(a) + Number(b);
+  return String(total);
+}
+
+/* 8 Kyu: Write a function that given a floor in the american system returns the floor in the european system. */
+
+function getRealFloor(n) {
+  if (n >= 13) {
+    return n - 2;
+  } else if (n <= 0) {
+    return n;
+  } else {
+    return n - 1;
+  }
+}
+
+/* 8 Kyu: Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity. */
+
+const flip = (d, a) => {
+  return d === "R" ? a.sort((a, b) => a - b) : a.sort((a, b) => b - a);
+};
