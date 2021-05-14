61bac084c613eb7678259a6e36076ca54561a7b3
Ryan Booth <ryanboothforever@gmail.com>
2021-05-14 09:10:42 -0500

    Codewars for 05_14

--- week-ending-05-15/main.js
+++ week-ending-05-15/main.js
@@ -41,3 +41,19 @@ function checkExam(array1, array2) {
   }
   return score;
 }
+
+/* 8 Kyu: Write a method, that will get an integer array as parameter and will process every number from this array. Return a new array with processing every number of the input-array like this:If the number has an integer square root, take this, otherwise square the number.
+[4,3,9,7,2,1] -> [2,9,3,49,4,1]
+The input array will always contain only positive numbers and will never be empty or null.
+The input array should not be modified! */
+
+function squareOrSquareRoot(array) {
+  let squared = array.map((item) => {
+    if (Math.sqrt(item) % 1 === 0) {
+      return Math.sqrt(item);
+    } else {
+      return item ** 2;
+    }
+  });
+  return squared;
+}
