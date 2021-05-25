9b91b322d94849a69517d5cd6919d292e0f3815b
Ryan Booth <ryanboothforever@gmail.com>
2021-05-25 11:45:11 -0500

    Last string wars

--- week-ending-05-29/main.js
+++ week-ending-05-29/main.js
@@ -70,3 +70,34 @@ function correctPolishLetters(string) {
   string = string.replace(/[ąćęłńóśźż]/g, (match) => letters[match]);
   return string;
 }
+
+/* 8 Kyu: Write a simple regex to validate a username. Allowed characters are:
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).*/
+
+function validateUsr(username) {
+  let regex = /[A-Z]/g;
+  return regex.test(username) == true ||
+    username.length < 4 ||
+    username.length > 16 ||
+    username.includes(" ")
+    ? false
+    : true;
+}
+
+/* better answer: function validateUsr(username) {
+  /**
+    - `^`        Start from the beginning of the string.
+    - `[]`       Allow any character specified, including...
+    - `a-z`      anything from a to z,
+    - `0-9`      anything from 0 to 9,
+    - `_`        and underscore.
+    - `{4,16}`   Accept 4 to 16 of allowed characters, both numbers included.
+    - `$`        End the string right after specified amount of allowed characters is given.
+
+  const validator = /^[a-z0-9_]{4,16}$/;
+  
+  return validator.test(username);
+}  */
