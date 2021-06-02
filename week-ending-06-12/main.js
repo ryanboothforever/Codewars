3af7f16ff3a5ffba146ec58b9e0ac3f878ccc1c7
Ryan Booth <ryanboothforever@gmail.com>
2021-06-02 09:53:33 -0500

    Movie theater rows codewars

--- week-ending-06-12/main.js
+++ week-ending-06-12/main.js
@@ -68,3 +68,9 @@ function getChar(c) {
 }
 
 /* 8 Kyu: Given the total number of rows and columns in the theater (nRows and nCols, respectively), and the row and column you're sitting in, return the number of people who sit strictly behind you and in your column or to the left, assuming all seats are occupied.*/
+
+function seatsInTheater(nCols, nRows, col, row) {
+  let colTotal = nCols - (col - 1);
+  let rowTotal = nRows - row;
+  return colTotal * rowTotal;
+}
