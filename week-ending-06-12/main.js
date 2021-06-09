d78b31d14a1351af20d07640abd1f17b3798bc45
Ryan Booth <ryanboothforever@gmail.com>
2021-06-09 10:02:26 -0500

    volume and area codewar

--- week-ending-06-12/main.js
+++ week-ending-06-12/main.js
@@ -131,3 +131,12 @@ function hello(name = "World") {
     return `Hello, ${person}!`;
   }
 }
+
+/* 8 Kyu: Write a function that returns the total surface area and volume of a box as an array: [area, volume]*/
+
+function getSize(width, height, depth) {
+  const area =
+    2 * (width * height) + 2 * (width * depth) + 2 * (height * depth);
+  const volume = width * height * depth;
+  return [area, volume];
+}
