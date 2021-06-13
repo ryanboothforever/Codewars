a54ac88bb86b1e68498f83a3ab2d966734b47021
Ryan Booth <ryanboothforever@gmail.com>
2021-06-13 09:01:20 -0500

    Code wars, function fix

--- week-ending-06-26/main.js
+++ week-ending-06-26/main.js
@@ -69,3 +69,10 @@ function replace(s) {
 function main(verb, noun) {
   return verb + noun;
 }
+
+/* 8 Kyu: I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number. */
+
+function addFive(num) {
+  var total = num + 5;
+  return total;
+}
