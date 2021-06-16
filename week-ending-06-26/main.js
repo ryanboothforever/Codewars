2240e96ab6fc02181b14574ebe26bb71f1dc1434
Ryan Booth <ryanboothforever@gmail.com>
2021-06-16 09:59:05 -0500

    Code wars, fundamentals

--- week-ending-06-26/main.js
+++ week-ending-06-26/main.js
@@ -65,4 +65,7 @@ function replace(s) {
   return replaced;
 }
 
-console.log(replace("Hello"));
+/* 8 Kyu: A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them. */
+function main(verb, noun) {
+  return verb + noun;
+}
