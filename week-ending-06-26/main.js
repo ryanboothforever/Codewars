4200a9ff96a0d3dcf6379fe4ea29476d27672035
Ryan Booth <ryanboothforever@gmail.com>
2021-06-19 08:06:21 -0500

    New week of wars 6_26

---
+++ week-ending-06-26/main.js
@@ -0,0 +1,19 @@
+/* 8 Kyu: Numbers ending with zeros are boring. They might be fun in your world, but not here. Get rid of them. Only the ending ones.*/
+function noBoringZeros(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n.toString().split("").pop() !== "0") {
+    return n;
+  } else {
+    let arr = n.toString().split("").reverse();
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > 0) {
+        arr.splice(0, i);
+        break;
+      }
+    }
+    return Number(arr.reverse().join(""));
+  }
+}
+
+console.log(noBoringZeros(2050));
