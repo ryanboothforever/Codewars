2c8529902546e7b358f261012e0cc2034d494eef
Ryan Booth <ryanboothforever@gmail.com>
2021-06-25 06:22:46 -0500

    Typeof operator problem

--- week-ending-06-26/main.js
+++ week-ending-06-26/main.js
@@ -106,3 +106,9 @@ function unusualFive() {
   const arr = ["a", "p", "p", "l", "e", "s"];
   return arr.indexOf("s");
 }
+
+/* 8 Kyu: Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".*/
+
+function problem(x) {
+  return typeof x === "string" ? "Error" : x * 50 + 6;
+}
