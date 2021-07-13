17fb3ae0c218ddbc91c09f1d4e33e7c5ea0433d5
Ryan Booth <ryanboothforever@gmail.com>
2021-07-13 10:22:12 -0500

    Return middle character or characters, charAt and Slice

--- week-ending-07-17/main.js
+++ week-ending-07-17/main.js
@@ -18,3 +18,16 @@ function middleMe(N, X, Y) {
   let addZ = repeatedArr.splice(middle, 0, "z");
   return repeatedArr.join("");
 }
+
+/* 7 Kyu: You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.*/
+
+function getMiddle(s) {
+  let arr = s.split("");
+  if (arr.length === 1) {
+    return s;
+  } else if (arr.length % 2 !== 0) {
+    return arr.join("").charAt(arr.length / 2);
+  } else {
+    return arr.join("").slice(arr.length / 2 - 1, arr.length / 2 + 1);
+  }
+}
