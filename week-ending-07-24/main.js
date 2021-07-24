1a3440d20b1637a9e171aef4b66c72b07be3fe39
Ryan Booth <ryanboothforever@gmail.com>
2021-07-23 22:16:42 -0500

    string rearrange attempt

--- week-ending-07-24/main.js
+++ week-ending-07-24/main.js
@@ -92,3 +92,21 @@ const nameShuffler = (str) => {
   const arr = str.split(" ");
   return `${arr[1]} ${arr[0]}`;
 };
+
+/* Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
+
+Test.assertSimilar(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+Test.assertSimilar(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']); 
+Test.assertSimilar(last('take me to semynak'), ['take', 'me', 'semynak', 'to']); */
+
+const alphaByFinalChar = (x) => {
+  let arr = x.split(" ").map((el) => el.toLowerCase());
+  let newArr = arr.reduce((cur, acc) => {
+    return cur.charAt(cur.length - 1) < acc.charAt(acc.length - 1) ? cur : acc;
+  }, "\u0434");
+  return newArr;
+};
