3bbfb3340599a70b7cbc16859974be21e4ea8a47
Ryan Booth <ryanboothforever@gmail.com>
2021-07-27 09:44:34 -0500

    Repeat string n amount of times

--- week-ending-07-31/main.js
+++ week-ending-07-31/main.js
@@ -39,4 +39,13 @@ const position = (str) => {
     }
   }
 };
-console.log(position("d"));
+
+/* 8 Kyu: Create a function that takes a string and an integer (n).
+
+The function should return a string that repeats the input string n number of times.
+
+If anything other than a string is passed in you should return "Not a string"*/
+
+const repeatIt = (str, n) => {
+  return typeof str === "string" ? str.repeat(n) : "Not a string";
+};
