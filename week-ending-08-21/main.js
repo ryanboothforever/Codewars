3f94221b6caf7ee37e5fd69276c640ca01eb4eb5
Ryan Booth <ryanboothforever@gmail.com>
2021-08-06 10:34:00 -0500

    Find remainder, but divide bigger num by smaller num

--- week-ending-08-21/main.js
+++ week-ending-08-21/main.js
@@ -59,4 +59,11 @@ function reverseWordsInPlace(str) {
   return newArr.join(" ");
 }
 
-/* 8 Kyu: Write a function that accepts two arguments and returns the remainder after dividing the larger number by the smaller number. Division by zero should return NaN (in C#, throw a new DivideByZeroException instead). Arguments will both be integers.*/
+/* 8 Kyu: Write a function that accepts two arguments and returns the remainder after dividing the larger number by the smaller number. Division by zero should return NaN. Arguments will both be integers.*/
+
+function remainder(a, b) {
+  if (a === 0 || b === 0) {
+    return NaN;
+  }
+  return a > b ? a % b : b % a;
+}
