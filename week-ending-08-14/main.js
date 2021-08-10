6f86dd940343f2a2bd442b3623611a6191862660
Ryan Booth <ryanboothforever@gmail.com>
2021-08-10 09:32:38 -0500

    shark attack math

--- week-ending-08-14/main.js
+++ week-ending-08-14/main.js
@@ -17,3 +17,32 @@ function lowercaseCount(str) {
   let count = str.match(regex);
   return count === null ? 0 : count.length;
 }
+
+/* 8 Kyu: You need to work out if the shark will get to you before you can get to the pontoon. To make it easier... as you do the mental calculations in the water you either freeze when you realise you are dead, or swim when you realise you can make it!
+
+You are given 5 variables;
+
+sharkDistance = distance from the shark to the pontoon. The shark will eat you if it reaches you before you escape to the pontoon.
+
+sharkSpeed = how fast it can move in metres/second.
+
+pontoonDistance = how far you need to swim to safety in metres.
+
+youSpeed = how fast you can swim in metres/second.
+
+dolphin = a boolean, if true, you can half the swimming speed of the shark as the dolphin will attack it.
+
+The pontoon, you, and the shark are all aligned in one dimension.
+
+If you make it, return "Alive!", if not, return "Shark Bait!".*/
+
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) {
+  if (dolphin === true) {
+    sharkSpeed = sharkSpeed / 2;
+  }
+  if (pontoonDistance / youSpeed < sharkDistance / sharkSpeed) {
+    return "Alive!";
+  } else {
+    return "Shark Bait!";
+  }
+}
