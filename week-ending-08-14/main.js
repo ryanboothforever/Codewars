4ae2a97be4305f8dca55f29bc75a98a39f9898b2
Ryan Booth <ryanboothforever@gmail.com>
2021-08-13 12:50:28 -0500

    Fighter winner ternary

--- week-ending-08-14/main.js
+++ week-ending-08-14/main.js
@@ -71,3 +71,17 @@ function reverseWithoutReverse(arr) {
   }
   return newArr;
 }
+
+/* 8 Kyu: If the winner is George Saint Pierre he will obviously say:
+
+"I am not impressed by your performance."
+If the winner is Conor McGregor he will most undoubtedly say:
+
+"I'd like to take this chance to apologize.. To absolutely NOBODY!"
+Good Luck!*/
+
+var quote = function (fighter) {
+  return fighter.toLowerCase() === "george saint pierre"
+    ? "I am not impressed by your performance."
+    : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
