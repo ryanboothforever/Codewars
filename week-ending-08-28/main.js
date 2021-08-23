1653d88c62bdc263c3631b2416e439a617f241f7
Ryan Booth <ryanboothforever@gmail.com>
2021-08-23 16:45:10 -0500

    Fixed template literal

--- week-ending-08-28/main.js
+++ week-ending-08-28/main.js
@@ -13,3 +13,9 @@ function isLockNessMonster(s) {
     return false;
   }
 }
+
+/* 8 Kyu: Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!*/
+
+function buildString(...template) {
+  return `I like ${template.join(", ")}!`;
+}
