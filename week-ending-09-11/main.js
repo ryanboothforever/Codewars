438b57da63f6b7bf9366b96671c9bb268ea25f25
Ryan Booth <ryanboothforever@gmail.com>
2021-09-07 10:22:59 -0500

    Money convert with toFixed

--- week-ending-09-11/main.js
+++ week-ending-09-11/main.js
@@ -45,3 +45,10 @@ function remove(s) {
   }
   return arr.join("");
 }
+
+/* 8 Kyu: Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'*/
+
+function usdcny(usd) {
+  let cny = usd * 6.75;
+  return `${cny.toFixed(2)} Chinese Yuan`;
+}
